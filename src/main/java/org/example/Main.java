b5fac45d2d2ce56aa28a16fddd37377c1a97226e
aliyzbsi <aliyzbsi68@hotmail.com>
2024-11-20 18:48:00 +0300

    s14-taskend

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,29 @@
 package org.example;
 
+import org.example.model.*;
+import org.example.model.enums.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Wall wall1 = new Wall("North");
+        Wall wall2 = new Wall("South");
+        Wall wall3 = new Wall("East");
+        Wall wall4 = new Wall("West");
+
+        Ceiling ceiling = new Ceiling(10, PaintColor.WHITE);
+        Bed bed = new Bed("Modern", 4, 3, 2, 1);
+        Lamp lamp = new Lamp(LampType.NEON, true, 75);
+        Wardrobe wardrobe = new Wardrobe(100, 200, 75.5);
+        Carpet carpet = new Carpet(50, 100, PaintColor.RED);
+
+        Bedroom bedroom = new Bedroom("My Room", wall1, wall2, wall3, wall4, ceiling, bed, lamp, wardrobe, carpet);
+
+        System.out.println("Bedroom Name: " + bedroom.getName());
+        bedroom.getBed().make();
+        bedroom.getLamp().turnOn();
+        bedroom.getCarpet().lying();
+        bedroom.getWardrobe().add();
+        bedroom.getCeiling().create();
+        bedroom.getWall1().create();
     }
 }
